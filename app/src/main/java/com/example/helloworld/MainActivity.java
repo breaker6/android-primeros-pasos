e53985e6884881aa72b3bd5ebb502169c380beec
breaker6 <62377366+breaker6@users.noreply.github.com>
2021-06-06 19:57:33 +0200

    Añadido botón que lleva a la ventana secundaria

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -14,7 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void iniciarActivitySecundario(View view) {
+    //Muestra un mensaje emergente en pantalla
+    public void mensajeActivitySecundario(View view) {
+        //Toast.makeText saca el mensaje en pantalla que recibe como parámetro en la misma pantalla
         Toast.makeText(this, "Entrando en el activity secundario", Toast.LENGTH_SHORT).show();
+    }
+
+    //Cargamos la pantalla conocida como ActivitySecundario
+    public void iniciarActivitySecundario(View view) {
+        //Declaramos el Intent que corresconderá a la pantalla o activity que queremos cargar
+        //Habrá que declarar su clase
+        Intent intentSecundario = new Intent(this, ActivitySecundario.class);
+        //Con el metodo startActivity lo ejecutaremos
+        startActivity(intentSecundario);
     }
 }
